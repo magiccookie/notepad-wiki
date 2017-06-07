602c4be01ab801c099db42a62cc5d4e182119a58
Magic Cookie <mgck@tuta.io>
2017-06-07 09:00:41 +0200

    handle split on click

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -8,11 +8,29 @@ import { Grid, Segment } from 'semantic-ui-react';
 import './style.css';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    const isSplit = true;
 
+  constructor(props) {
+    super(props);
+    this.state = { isSplit: false };
+  }
+
+  toggleSplit = () => {
+    const curSplit = this.state.isSplit;
+    this.setState({ isSplit: !curSplit });
+  }
+
+  handleLink = (e) => {
+    e.preventDefault();
+    this.toggleSplit();
+  }
+
+  render() {
     return (
-      <Grid columns={2} padded="vertically" centered>
+      <Grid
+        columns={2}
+        padded="vertically"
+        centered={!this.state.isSplit}
+      >
         <Grid.Row>
           <Grid.Column>
             <Segment>
@@ -22,10 +40,13 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
                 molestias est, excepturi magni dolore voluptatem assumenda fugiat nulla tempora dicta quasi sunt. Lorem ipsum
                 dolor sit amet, consectetur adipisicing elit. Ad beatae quam sequi, nihil perferendis illum doloribus asperiores
                 molestias est, excepturi magni dolore voluptatem assumenda fugiat nulla tempora dicta quasi sunt.
+                <a href="#note2" onClick={(e) => this.handleLink(e)}>
+                  Link to another note
+                </a>
               </p>
             </Segment>
           </Grid.Column>
-          { isSplit ?
+          { this.state.isSplit ?
             (
               <Grid.Column>
                 <Segment>
