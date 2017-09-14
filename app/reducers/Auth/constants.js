73b75f15a674b47ea46075b50d8f3f192301922b
Magic Cookie <mgck@tuta.io>
2017-09-14 02:18:41 +0200

    quick fix

--- app/reducers/Auth/constants.js
+++ app/reducers/Auth/constants.js
@@ -6,7 +6,6 @@
 
 export const AUTHORIZE = 'reducers/Auth/AUTHORIZE';
 export const UNAUTHORIZED = 'reducers/Auth/UNAUTHORIZED';
-export const WIPE_TMP_CREDITS = 'reducers/Auth/WIPE_TMP_CREDITS';
 
 export const CHANGE_AUTH_STATE = 'reducers/Auth/CHANGE_AUTH_STATE';
 export const WIPE_AUTH_STATE = 'reducers/Auth/WIPE_AUTH_STATE';
