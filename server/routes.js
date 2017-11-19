d20d81e2ee31b930411f8f07af5dbe7654484b99
Artiem K <artiemq@gmail.com>
2017-11-19 20:18:45 +0300

    add users and notes validation

--- server/routes.js
+++ server/routes.js
@@ -17,8 +17,8 @@ const crypto = require('crypto');
 
 const secret = process.env.NOTEPAD_WIKI_KEY || 'notepad-wiki';
 const encSHA512 = (value) => crypto.createHmac('sha512', secret)
-                                   .update(value)
-                                   .digest('hex');
+  .update(value)
+  .digest('hex');
 
 const postToken = (opts) => (req, res) => {
   if (req.body.username && req.body.password) {
@@ -29,33 +29,59 @@ const postToken = (opts) => (req, res) => {
     client.query(`SELECT 1
                   FROM users
                   WHERE username=$1
-                  AND hash=$2`,
-                 [username, hash])
-          .then((data) => {
-            if (data.rows.length) {
-              const payload = { username: username };
-              const token = jwt.encode(payload, opts.secretOrKey, opts.algorithms[0]);
-              res.status(200).json({ token: token });
-            } else {
-              return res.status(401)
-            }
-          })
-          .catch((err) => {
-            console.log(err);
-            return res.status(401).json(err);
+                  AND hash=$2`, [username, hash])
+      .then((data) => {
+        if (data.rows.length) {
+          const payload = {
+            username: username
+          };
+          const token = jwt.encode(payload, opts.secretOrKey, opts.algorithms[0]);
+          res.status(200).json({
+            token: token
           });
+        } else {
+          return res.status(401)
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(401).json(err);
+      });
   }
 }
 
 
 const verifyChallenge = (challenge) => true;
-const verifyUser = (username) => true;
+const verifyUser = (username) => {
+  client.query(`EXISTS (SELECT 1
+                FROM users
+                WHERE username=$1`, [username])
+    .then(result => {
+      if (result) {
+        throw {'error':'user already exists'}
+      }
+    })
+    .catch(e => e)
+}
+
+const verifyNote = (name) => {
+  if (!utils.validate_name(name)){
+    return false          
+  }
+  client.query(`EXISTS (SELECT 1
+                FROM notes
+                WHERE name=$1`, [name])
+    .then(result => {
+      return !result
+    })
+    .catch(e => e)
+}
 
 const postSignup = (req, res) => {
   if (req.body.username && req.body.password) {
-    const username  = req.body.username.trim().toLowerCase();
-    const email     = req.body.email.trim().toLowerCase();
-    const password  = req.body.password.trim();
+    const username = req.body.username.trim().toLowerCase();
+    const email = req.body.email.trim().toLowerCase();
+    const password = req.body.password.trim();
     const challenge = req.body.challenge || '';
 
     if (username && password && challenge) {
@@ -65,15 +91,14 @@ const postSignup = (req, res) => {
         const hash = encSHA512(password);
         client.query(`INSERT
                       INTO users(username, hash, email)
-                      values($1, $2, $3)`,
-                     [username, hash, email])
-              .then((data) => {
-                return res.status(200).json(data);
-              })
-              .catch((err) => {
-                console.log(err);
-                return res.status(401).json(err);
-              });
+                      values($1, $2, $3)`, [username, hash, email])
+          .then((data) => {
+            return res.status(200).json(data);
+          })
+          .catch((err) => {
+            console.log(err);
+            return res.status(401).json(err);
+          });
       } catch (err) {
         return res.status(401).json(err);
       }
@@ -82,101 +107,101 @@ const postSignup = (req, res) => {
 }
 
 router.route("/posts/:note_id?")
-      .get((req, res, next) => {
-        const owner = req.user;
-        if (req.query.name) {
-          const name = req.query.name;
-          client.query(`SELECT *
+  .get((req, res, next) => {
+    const owner = req.user;
+    if (req.query.name) {
+      const name = req.query.name;
+      client.query(`SELECT *
                         FROM notes
                         WHERE (owner IS NULL OR owner=$1)
-                        AND name=$2`,
-                       [owner, name])
-                .then((data) => {
-                  return res.status(200).json(data.rows)
-                })
-                .catch((err) => {
-                  return res.status(500).json(err);
-                })
-        } else {
-          client.query(`SELECT *
+                        AND name=$2`, [owner, name])
+        .then((data) => {
+          return res.status(200).json(data.rows)
+        })
+        .catch((err) => {
+          return res.status(500).json(err);
+        })
+    } else {
+      client.query(`SELECT *
                         FROM notes
-                        WHERE (owner IS NULL OR owner=$1)`,
-                       [owner])
-                .then((data) => {
-                  return res.status(200).json(data.rows)
-                })
-                .catch((err) => {
-                  return res.status(500).json(err);
-                })
-        }
-      })
-      .post((req, res, next) => {
-        const owner = req.user;
-        const note = {
-          owner: owner,
-          name: req.body.name,
-          header: req.body.header,
-          content: req.body.content,
-          createdAt: new Date().toISOString(),
-          editedAt: new Date().toISOString(),
-        };
+                        WHERE (owner IS NULL OR owner=$1)`, [owner])
+        .then((data) => {
+          return res.status(200).json(data.rows)
+        })
+        .catch((err) => {
+          return res.status(500).json(err);
+        })
+    }
+  })
+  .post((req, res, next) => {
+    const owner = req.user;
+    const note = {
+      owner: owner,
+      name: req.body.name,
+      header: req.body.header,
+      content: req.body.content,
+      createdAt: new Date().toISOString(),
+      editedAt: new Date().toISOString(),
+    };
 
-        if (!utils.validate_name(note.name)){
-          return res.status(500).json({"error":"invalid name"});          
-        }
-        client.query(`INSERT
+    if (validateNote(note.name)) {
+      return res.status(500).json({
+        "error": "invalid name"
+      });
+    }
+    client.query(`INSERT
                       INTO notes(owner, name, header, content, createdAt, editedAt)
-                      values($1, $2, $3, $4, $5, $6)`,
-                     [note.owner, note.name, note.header, note.content, note.createdAt, note.editedAt])
-              .then((data) => {
-                return res.status(200).json(data);
-              })
-              .catch((err) => {
-                console.log(err);
-                return res.status(500).json(err);
-              });
+                      values($1, $2, $3, $4, $5, $6)`, [note.owner, note.name, note.header, note.content, note.createdAt, note.editedAt])
+      .then((data) => {
+        return res.status(200).json(data);
       })
-      .put((req, res) => {
-        const owner = req.user;
-        const note_id = req.params.note_id;
-        const note = {
-          name: req.body.name,
-          header: req.body.header,
-          content: req.body.content,
-          editedAt: new Date().toISOString(),
-        };
-        if (!utils.validate_name(note.name)){
-          return res.status(500).json({"error":"invalid name"});          
-        }
-        client.query(`UPDATE notes
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  })
+  .put((req, res) => {
+    const owner = req.user;
+    const note_id = req.params.note_id;
+    const note = {
+      name: req.body.name,
+      header: req.body.header,
+      content: req.body.content,
+      editedAt: new Date().toISOString(),
+    };
+    if (!verifyNote(note.name)) {
+      return res.status(500).json({
+        "error": "invalid name"
+      });
+    }
+    client.query(`UPDATE notes
                       SET name=($1), header=($2), content=($3), editedAt=($4)
                       WHERE id=($5)
-                      AND (owner IS NULL OR owner=($6))`,
-                     [note.name, note.header, note.content, note.editedAt, note_id, owner])
-              .then((data) => {
-                return res.status(200).json(data);
-              })
-              .catch((err) => {
-                console.log(err);
-                return res.status(500).json(err);
-              });
+                      AND (owner IS NULL OR owner=($6))`, [note.name, note.header, note.content, note.editedAt, note_id, owner])
+      .then((data) => {
+        return res.status(200).json(data);
       })
-      .delete((req, res, next) => {
-        const owner = req.user;
-        const note_id = req.params.note_id;
-        client.query(`DELETE
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  })
+  .delete((req, res, next) => {
+    const owner = req.user;
+    const note_id = req.params.note_id;
+    client.query(`DELETE
                       FROM notes
                       WHERE id=($1)
                       AND (owner IS NULL OR owner=($2))`, [note_id, owner])
-              .then((data) => {
-                return res.status(200).json(data);
-              })
-              .catch((err) => {
-                console.log(err);
-                return res.status(500).json(err);
-              })
-      });
+      .then((data) => {
+        return res.status(200).json(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      })
+  });
 
 exports.router = router;
-exports.token  = postToken;
+exports.token = postToken;
 exports.signup = postSignup;
